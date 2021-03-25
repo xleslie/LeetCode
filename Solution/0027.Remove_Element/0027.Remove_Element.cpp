0e24c55d35c0f60aa7c3add7014fea36a1ef255b
XYD <41739224+xleslie@users.noreply.github.com>
2021-03-25 11:41:50 +0800

    Create 0027.Remove_Element.cpp

---
+++ Solution/0027.Remove_Element/0027.Remove_Element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if(nums.size()==0)
+            return 0;
+        int i=0,j=0;
+        while(j<nums.size())
+        {
+            if(nums[j]!=val)
+            {
+                if(j!=i)
+                    nums[i]=nums[j];
+                ++i;
+            }
+            ++j;        
+        }
+        return i;
+    }
+};
