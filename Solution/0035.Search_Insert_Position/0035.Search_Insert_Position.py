f4917df1d49c1bb8a6ae6eb058b45ce91a4b93cc
XYD <41739224+xleslie@users.noreply.github.com>
2021-03-27 02:12:03 +0800

    Create 0035.Search_Insert_Position.py

---
+++ Solution/0035.Search_Insert_Position/0035.Search_Insert_Position.py
@@ -0,0 +1,12 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        i,j=0,len(nums)
+        while i<j:
+            m=i+(j-i)//2
+            if nums[m]==target:
+                return m
+            elif nums[m]>target:
+                j=m
+            elif nums[m]<target:
+                i=m+1
+        return i
