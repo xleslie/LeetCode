451d0bebc10f366a69a5c8fc0ef6bbd5b3888b1c
XYD <41739224+xleslie@users.noreply.github.com>
2021-03-27 19:05:09 +0800

    Update 0167.Two Sum II - Input array is sorted.cpp

--- Solution/0167.Two Sum II - Input_array_is_sorted/0167.Two Sum II - Input array is sorted.cpp
+++ Solution/0167.Two Sum II - Input_array_is_sorted/0167.Two Sum II - Input array is sorted.cpp
@@ -1,3 +1,4 @@
+//法一：双指针法
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
@@ -9,6 +10,29 @@ public:
             else if(numbers[i]+numbers[j]<target)
                 ++i;
             else
+                --j;
+        }
+        return {};
+    }
+};
+//法二：二分查找法+双指针法
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i=0,j=numbers.size()-1;
+        int m;
+        while(i<j)
+        {
+            m= i+(j-i)/2;
+            if(numbers[i]+numbers[j]==target)
+                return {i+1,j+1};
+            else if(numbers[i]+numbers[m]>target)
+                j=m-1;
+            else if(numbers[m]+numbers[j]<target)
+                i=m+1;
+            else if(numbers[i]+numbers[j]<target)
+                ++i;
+            else if(numbers[i]+numbers[j]>target)
                 --j;
         }
         return {};
